1993254c74c78f282ac19657ccb75be0fc955ff1
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-04 07:58:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 board  = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(array)
-  puts "   |   |   "
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
+
